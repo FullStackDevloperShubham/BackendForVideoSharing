c465af4eb975dd4b1f9c9bd5e6910dd3b28e10e8
FullStackDeveloperShubham <githubshubhamac@gmail.com>
2025-03-18 18:49:21 +0530

    bcrypt js added . refresh token and access tokenn generated

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from 'mongoose'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 const userSchema = new Schema(
   {
@@ -47,5 +49,49 @@ const userSchema = new Schema(
   },{
     timestamps: true,
   })
+
+
+  // hash the password before saving into the database
+userSchema.pre("save",async function (next){
+
+  if(!this.modified("password")) return next()
+
+  this.password = bcrypt.hash(this.password,10)
+
+  next()
+})
+
+// matching the password
+userSchema.methods.isPasswordCorrect () = async function(password){
+ return  await bcrypt.compare(password , this.password)
+}
+
+// generating access token
+userSchema.methods.generateAccessToken = function(){
+  // short lived access token
+  return jwt.sign(
+    { 
+        _id : this._id,
+        email : this.email,
+        fullname : this.fullname
+     }, 
+     process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn:  process.env.ACCESS_TOKEN_EXPIRY}
+  );
+}
+
+// generate generateRefreshToken
+userSchema.methods.generateRefreshToken = function(){
+  // short lived access token
+  return jwt.sign(
+    { 
+        _id : this._id,
+     }, 
+     process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn:  process.env.REFRESH_TOKEN_EXPIRY}
+  );
+}
+
+
 
 export const User = mongoose.model("User", userSchema)
