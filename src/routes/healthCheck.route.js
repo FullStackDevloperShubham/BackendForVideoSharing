1ebf580699a5c9f6770d3cc944d91656425553cf
FullStackDeveloperShubham <githubshubhamac@gmail.com>
2025-03-20 11:51:26 +0530

    cloudinary and multer setup

--- src/routes/healthCheck.route.js
+++ src/routes/healthCheck.route.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { helthCheck } from '../controllers/helthCheck.controller.js'
+import { upload } from '../middlewares/multer.midlleware.js'
 
 const router = Router()
 
